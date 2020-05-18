b83e9688b09881c0633e4cf7bb6ca893761d699f
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2020-05-19 01:24:47 +0200

    feat: Init index page with old content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  theme: {},
+  theme: {
+    container: {
+      padding: {
+        default: '1rem',
+        sm: '4rem',
+        lg: '8rem',
+        xl: '12rem',
+      },
+    },
+  },
   variants: {},
   plugins: [],
 }
